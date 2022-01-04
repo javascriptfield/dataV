f31e4f42cfa57da1d129f260e3c725df53113e32
15190562801 <15190562801@163.com>
2022-01-04 12:40:59 +0800

    引入sass和windicss

--- src/views/Shangpinfang.vue
+++ src/views/Shangpinfang.vue
@@ -10,44 +10,66 @@ import GaugeChartContainer from "../components/GaugeChartContainer/index.vue";
 import TrendChart from "../components/TrendChart/index.vue";
 import SalesStatisticsChart from "../components/SalesStatisticsChart/index.vue";
 import StatisticsTable from "../components/StatisticsTable/index.vue";
-const titleHeight = (100 / 1080) * 80 + "vh";
-const mapHeight = (100 / 1080) * 1000 + "vh";
-const graphHeight = (100 / 1080) * 300 + "vh";
-const fontSize = (document.body.clientWidth / 1920) * 18 + "px";
 </script>
 
 <template>
-  <div class="container">
-    <div class="header">商品房交易可视化平台</div>
-    <div class="graph-container">
-      <div class="left-container">
-        <div class="image-border">
-          <span class="graph-title">价格分段销售占比</span>
+  <div class="w-full h-full text-center">
+    <div class="header-container flex flex-col items-center justify-center">
+      <span class="header-text font-bold truncate">商品房交易可视化平台</span>
+    </div>
+    <div class="flex">
+      <div>
+        <div
+          class="image-border relative flex flex-col items-center justify-center"
+        >
+          <span class="graph-title truncate absolute top-0 font-bold"
+            >价格分段销售占比</span
+          >
           <PricePieChart />
         </div>
-        <div class="image-border">
-          <span class="graph-title">当前销量及库存</span>
+        <div
+          class="image-border relative flex flex-col items-center justify-center"
+        >
+          <span class="graph-title truncate absolute top-0 font-bold"
+            >当前销量及库存</span
+          >
           <GaugeChartContainer />
         </div>
-        <div class="image-border">
-          <span class="graph-title">面积分段销量占比</span>
+        <div
+          class="image-border relative flex flex-col items-center justify-center"
+        >
+          <span class="graph-title truncate absolute top-0 font-bold"
+            >面积分段销量占比</span
+          >
           <AreaPieChart />
         </div>
       </div>
       <div class="map">
         <MapChart />
       </div>
-      <div class="right-container">
-        <div class="image-border">
-          <span class="graph-title">全年趋势</span>
+      <div>
+        <div
+          class="image-border relative flex flex-col items-center justify-center"
+        >
+          <span class="graph-title truncate absolute top-0 font-bold"
+            >全年趋势</span
+          >
           <TrendChart />
         </div>
-        <div class="image-border">
-          <span class="graph-title">上月销售统计</span>
+        <div
+          class="image-border relative flex flex-col items-center justify-center"
+        >
+          <span class="graph-title truncate absolute top-0 font-bold"
+            >上月销售统计</span
+          >
           <StatisticsTable />
         </div>
-        <div class="image-border">
-          <span class="graph-title">各区销量统计</span>
+        <div
+          class="image-border relative flex flex-col items-center justify-center"
+        >
+          <span class="graph-title truncate absolute top-0 font-bold"
+            >各区销量统计</span
+          >
           <SalesStatisticsChart />
         </div>
       </div>
@@ -55,52 +77,32 @@ const fontSize = (document.body.clientWidth / 1920) * 18 + "px";
   </div>
 </template>
 
-<style>
-.header {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  height: v-bind(titleHeight);
+<style lang="scss" scoped>
+@import "../style/function.scss";
+.header-container {
+  height: pxToVh(80);
   box-shadow: 0 -7px 30px -8px rgba(40, 174, 195, 0.64) inset;
-  font-size: calc(v-bind(fontSize) * 2);
-  color: #28aec3;
-  font-weight: bold;
   margin-bottom: 20px;
 }
-.graph-container {
-  display: flex;
-  width: 100%;
+.header-text {
+  font-size: pxToVh(36);
+  color: #28aec3;
+  width: 360px;
 }
 .map {
   width: 760px;
-  height: v-bind(mapHeight);
-}
-.gauge-container {
-  display: flex;
+  height: pxToVh(1000);
 }
 .image-border {
-  position: relative;
   width: 541px;
-  height: v-bind(graphHeight);
+  height: pxToVh(300);
   background: url("../src/assets/images/border.png") no-repeat center;
   background-size: 100% 100%;
   margin: 0 20px 20px 20px;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
 }
 .graph-title {
   color: #28aec3;
-  font-size: v-bind(fontSize);
-  font-weight: bold;
+  font-size: pxToVh(18);
   width: 144px;
-  position: absolute;
-  top: 0;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  text-align: center;
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), WindiCSS()],
   server: {
     host: '0.0.0.0',
   },
@@ -17,6 +17,7 @@ export default defineConfig({
           viewportUnit: 'vw',
           fontViewportUnit: 'vw',
         })
+
       ]
     }
   }
