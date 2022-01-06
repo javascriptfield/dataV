bd9d7b2e65e11da9117f84800fb4fa54375660e0
15190562801 <15190562801@163.com>
2022-01-06 17:39:26 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import { viteMockServe } from 'vite-plugin-mock'
 export default defineConfig({
-  plugins: [vue(), WindiCSS(), viteMockServe({
-    mockPath: 'mock',
-    localEnabled: true,
-    prodEnabled: true,
-    injectCode: `
-      import { setupProdMockServer } from './mockProdServer.js';
-      setupProdMockServer()
-    `
-  })],
+  plugins: [vue(), WindiCSS()],
   server: {
     host: '0.0.0.0',
     proxy: {
