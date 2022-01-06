18172052eaf586fcd581866c418b5f5553c72ad6
15190562801 <15190562801@163.com>
2022-01-06 17:04:00 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'virtual:windi.css';
 import { setupProdMockServer } from './mockProdServer';
+console.log('import.meta.env.MODE: ', import.meta.env.MODE);
 if (import.meta.env.MODE === 'development') {
   setupProdMockServer()
 }
