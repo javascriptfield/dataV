b63cb5b999011d0ad4632c99ac5640b0bdc46cb1
15190562801 <15190562801@163.com>
2022-01-06 17:19:33 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'virtual:windi.css';
-console.log('import.meta.env.MODE: ', import.meta.env.MODE);
-// import { setupProdMockServer } from './mockProdServer';
-// if (import.meta.env.MODE === 'development') {
-//   setupProdMockServer()
-// }
+import { setupProdMockServer } from './mockProdServer';
+if (import.meta.env.MODE === 'development') {
+  setupProdMockServer()
+}
 createApp(App).mount('#app')
