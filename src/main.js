3b32ce7b6ce7c13877e011a1c8f8ecc4e637e707
15190562801 <15190562801@163.com>
2022-01-06 17:13:27 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'virtual:windi.css';
-import { setupProdMockServer } from './mockProdServer';
-console.log('import.meta.env.MODE: ', import.meta.env.MODE);
-if (import.meta.env.MODE === 'development') {
-  setupProdMockServer()
-}
+// import { setupProdMockServer } from './mockProdServer';
+// if (import.meta.env.MODE === 'development') {
+//   setupProdMockServer()
+// }
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -2,48 +2,38 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import { viteMockServe } from 'vite-plugin-mock'
-export default ((command) => {
-  let prodMock = true
-  return {
-    plugins: [vue(), WindiCSS(), viteMockServe({
-      ignore: /^\_/,
-      mockPath: 'mock',
-      localEnabled: !prodMock,
-      prodEnabled: prodMock,
-      injectCode: `
-         import { setupProdMockServer } from './src/mockProdServer.js';
-   
-         setupProdMockServer();
-         `,
-    })],
-    server: {
-      host: '0.0.0.0',
-      proxy: {
-        '/ZsExternalInterface': {
-          target: 'http://192.168.1.140:9619',
-          changeOrigin: true,
-        },
-      }
-    },
-    css: {
-      postcss: {
-        plugins: [
-          require('postcss-px-to-viewport')({
-            unitToConvert: 'px',
-            viewportWidth: 1920,
-            propList: ['*', "!height", "!font-size"],
-            viewportUnit: 'vw',
-            fontViewportUnit: 'vw',
-          })
-
-        ]
+export default defineConfig({
+  plugins: [vue(), WindiCSS(), viteMockServe({
+    mockPath: 'mock',
+    localEnabled: true,
+    prodEnabled: true,
+  })],
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/ZsExternalInterface': {
+        target: 'http://192.168.1.140:9619',
+        changeOrigin: true,
       },
-      preprocessorOptions: {
-        scss: {
-          additionalData: ["@import './src/style/index.scss';"]
-        }
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        require('postcss-px-to-viewport')({
+          unitToConvert: 'px',
+          viewportWidth: 1920,
+          propList: ['*', "!height", "!font-size"],
+          viewportUnit: 'vw',
+          fontViewportUnit: 'vw',
+        })
+
+      ]
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: ["@import './src/style/index.scss';"]
       }
     }
   }
-
 })
